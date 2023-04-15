be4c866686ad4e06fd2be895524fba33e6531af5
suchat charoenpon <charoenpon@hotmail.com>
2023-04-15 13:02:12 +0700

    ดึงข้อมูลแบบ Query Builder

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -11,7 +11,7 @@ class DepartmentController extends Controller
 {
     public function index()
     {
-        $departments = Department::all();
+        $departments = DB::table('departments')->get();
         return view('admin.department.index',compact('departments'));
     }
 
