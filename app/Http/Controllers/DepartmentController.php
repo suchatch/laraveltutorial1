6b67984e13ed7877c65acfc99f4642bd23a97218
suchat charoenpon <charoenpon@hotmail.com>
2023-04-15 12:16:36 +0700

    ปรับแต่ง Error Message

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('admin.department.index');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
-            'department_name'=>'required|unique:departments|max:255'
+            'department_name' => 'required|unique:departments|max:255'
+        ], [
+            'department_name.required' => 'กรุณาป้อนชื่อ ชื่อแผนก',
+            'department_name.max' => 'ห้ามป้อนเกิน 255 ตัวอักษร'
         ]);
     }
 }
