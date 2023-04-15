a895557ed2bb418e2920e6ddc9f9aad70740fdae
suchat charoenpon <charoenpon@hotmail.com>
2023-04-15 13:26:16 +0700

    แก้ปัญหาการแสดงเลขลำดับ

--- resources/views/admin/department/index.blade.php
+++ resources/views/admin/department/index.blade.php
@@ -24,10 +24,9 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @php($i=1)
                                 @foreach($departments as $row)
                                 <tr>
-                                    <th>{{$i++}}</th>
+                                    <th>{{$departments->firstItem()+$loop->index}}</th>
                                     <td>{{$row->department_name}}</td>
                                     <td>{{$row->user_id}}</td>
                                 </tr>
